6d2a3284a8540a8037773436d044cb43bab9f0e1
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-06 08:14:54 -0600

    Canvi nom columna 'Name' de la taula 'Genre' a 'GenreName' ??
    Pèrdua d'informació si la taula Genre conté dades

--- Libraries/Model/Models/Genre.cs
+++ Libraries/Model/Models/Genre.cs
@@ -3,7 +3,7 @@ namespace UdemyCourse.Model.Models
     public class Genre
     {
         public int GenreId { get; set; }
-        public string Name { get; set; }
+        public string GenreName { get; set; }
         public int DisplayOrder { get; set; }
     }
 }
