1708284e89439fa22189e9131a1ead7b76b4efa6
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-06 03:34:05 -0600

    Eliminar carpeta Migracions i crear base de dades a Docker

--- run.bash
+++ run.bash
@@ -1,1 +1,5 @@
 sudo -S sh -c /home/projecteinf/Projectes/EF/Course/dockerRun.bash
+rm -r Migrations
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Passw0rd!"
