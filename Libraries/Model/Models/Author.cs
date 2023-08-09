76396d15d0ca59e7b9fb196bb40b849910afbc9d
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-09 04:04:35 -0600

    Afegida classe Author amb NotMapped i computed camp

--- Libraries/Model/Models/Author.cs
+++ Libraries/Model/Models/Author.cs
@@ -1,12 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 public class Author
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public dateTime BirthDate { get; set; }
+    public DateTime BirthDate { get; set; }
     public string Location { get; set; }
-    [notMapped]
+    [NotMapped]
     public string FullName { 
         get { 
             return $"{FirstName} {LastName}"; 
