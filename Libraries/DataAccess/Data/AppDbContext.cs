96a2e14d1f6c3ee39355f533848adcc9f5a3b1fd
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-09 04:08:12 -0600

    Afegida classe Publisher

--- Libraries/DataAccess/Data/AppDbContext.cs
+++ Libraries/DataAccess/Data/AppDbContext.cs
@@ -10,5 +10,6 @@ namespace UdemyCourse.DataAccess {
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
---
+++ Libraries/Model/Models/Publisher.cs
@@ -0,0 +1,7 @@
+public class Publisher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    
+}
