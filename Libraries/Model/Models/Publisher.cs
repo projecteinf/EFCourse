12b5a19ad281765877ad448030429d841606fdc5
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-09 04:13:19 -0600

    Generació valors PK Publisher no autoincrementals

--- Libraries/Model/Models/Publisher.cs
+++ Libraries/Model/Models/Publisher.cs
@@ -1,5 +1,7 @@
 public class Publisher
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)] // No autoincremental
     public int Id { get; set; }
     public string Name { get; set; }
     public string Location { get; set; }
