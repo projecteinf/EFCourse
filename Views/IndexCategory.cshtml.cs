5e2a52abfa7cd6287469db08b494b973cfd8977c
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-11 09:54:17 -0600

    Creació pàgines per a la visualització de les categories.
    dotnet new page --name IndexCategory --namespace UdemyCourse.Views --output Views

---
+++ Views/IndexCategory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UdemyCourse.Views
+{
+    public class IndexCategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
