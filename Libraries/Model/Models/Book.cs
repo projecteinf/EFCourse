b213d53493d55f02907a630a3e1725190038ef6f
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-11 02:28:51 -0600

    Relació 1 a 1 entre Book i BookDetail

--- Libraries/Model/Models/Book.cs
+++ Libraries/Model/Models/Book.cs
@@ -17,4 +17,8 @@ public class Book
     [ForeignKey("Category")] // Category és el nom de la taula a la que fa referència
     public int CategoryId { get; set; } // Propietat de navegació
     public Category Category { get; set; }  // Creació de la relació
+    // Relació 1 a 1
+    [ForeignKey("BookDetail")] 
+    public int BookDetailId { get; set; } // Propietat de navegació
+    public BookDetail BookDetail { get; set; }  // Creació de la relació
 }
--- Libraries/Model/Models/BookDetail.cs
+++ Libraries/Model/Models/BookDetail.cs
@@ -8,7 +8,9 @@ namespace UdemyCourse.Model.Models
         public int Id { get; set; }
         public int NumberOfChapters { get; set; }
         public int NumberOfPages { get; set; }
+        [Required]
         public string Language { get; set; }
         public double Weight { get; set; }
+        public Book Book { get; set; }
     }
 }
