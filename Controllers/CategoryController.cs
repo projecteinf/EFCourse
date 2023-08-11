c45167343653018bcbe62836bab3f41b2c8ea541
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-11 09:43:31 -0600

    Referència a DbContext dins del controlador de Category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UdemyCourse.DataAccess;
 
 namespace UdemyCourse.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
