6bade907351e2e505048af9811284e06847a1031
projecteinf <projecteinf@boscdelacoma.cat>
2023-08-11 01:54:56 -0600

    Added relationship between book and category

--- Libraries/Model/Models/Book.cs
+++ Libraries/Model/Models/Book.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using UdemyCourse.Model.Models;
 public class Book
 {
     [Key]
@@ -13,4 +14,5 @@ public class Book
     public double price { get; set; }
     [NotMapped] // Atribut de visualització
     public string PriceRange { get; set; }
+    public Category Category { get; set; }
 }
